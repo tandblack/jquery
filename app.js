fff54257013b7a03f66407837b05631a905f257e
Trent Blackman <trentablackman@gmail.com>
2020-10-19 19:41:10 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$(`.navItem`).css(`color`, `white`);
+
+$(`#nav`).css(`background-color`, `#144`);
+
+$(`li:even`).css(`background-color`, `#ccc`);
+
+$(`li:odd`).css(`background-color`, `#aaa`);
+
+$(`input`).val(`Trent`);
